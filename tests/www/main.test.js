7cd194bd8d5ad9d7cabca5b41ff3a9777d073a2a
philopian <pdotwdot@gmail.com>
2017-05-09 17:44:57 -0700

    updated the npm script to use the portTests property from the config instead of hard coded 8081

--- tests/www/main.test.js
+++ tests/www/main.test.js
@@ -6,11 +6,14 @@ import Main from '../../www/js/main.js';
 let m = {};
 before((done) => {
   m = new Main();
-  // console.log(`\n  ${m.html()} \n\n`);
+  console.log(`\n  ${m.html()} \n\n`);
   done();
 });
 
 describe('second tests', () => {
+  it('html() should return string of HTML', () => {
+    expect(m.html()).to.be.a('string');
+  });
   it('should pass', () => {
     expect(true).to.be.true;
   });
