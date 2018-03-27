0be12119179f94b97d4d179f1639a2d3977a0711
philopian <pdotwdot@gmail.com>
2018-03-27 10:48:48 -0700

    added fonts capability to storybook

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -11,6 +11,10 @@ module.exports = {
         test: /\.css$/,
         loaders: ["style-loader", "css-loader"],
         include: path.resolve(__dirname, '../')
+      },
+      {
+        test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+        loader: 'file-loader?name=fonts/[name].[ext]'
       }
     ]
   }
