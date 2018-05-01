6eea42360f66e343758fa9e2445e5598ac587ae3
philopian <pdotwdot@gmail.com>
2018-04-30 18:03:41 -0700

    updates

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,9 +3,9 @@ import { configure } from '@storybook/react';
 function loadStories() {
   require("babel-polyfill");
   require('../www/sass/global.scss');
-  require('../www/react/components/SimpleButton/storybook.js');
-  require('../www/react/components/StyleComponents/storybook.js');
-  require("../www/react/components/UserList/storybook.js");
+  // require('../www/react/components/SimpleButton/storybook.js');
+  // require('../www/react/components/StyleComponents/storybook.js');
+  // require("../www/react/components/UserList/storybook.js");
 } // DONT'T DELETE THIS
 
 configure(loadStories, module);
--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -2,14 +2,15 @@ const path = require('path');
 
 module.exports = {
   module: {
-    rules: [{
-        test: /\.scss$/,
-        loaders: ["style-loader", "css-loader", "sass-loader"],
-        include: path.resolve(__dirname, '../')
-      },
+    rules: [
       {
         test: /\.css$/,
         loaders: ["style-loader", "css-loader"],
+        include: path.resolve(__dirname, '../')
+      },
+      {
+        test: /\.scss$/,
+        loaders: ["style-loader", "css-loader", "sass-loader"],
         include: path.resolve(__dirname, '../')
       },
       {
